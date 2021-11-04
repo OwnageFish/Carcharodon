a7fa219926c0c00bde7bf9567b7425ee9cc82d15
a1ineman <alineman123456555@gmail.com>
2021-11-03 23:37:17 -0400

    Added inital project files. Save for the Carcharodon dir in .vs

---
+++ Carcharodon/Carcharodon.cpp
@@ -0,0 +1,12 @@
+﻿// Carcharodon.cpp : Defines the entry point for the application.
+//
+
+#include "Carcharodon.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
