a329e1dbfdb11e4c269c23c9cbaa13e4ed98855a
a1ineman <alineman123456555@gmail.com>
2021-11-08 13:48:35 -0500

    Added a little to BspViewer functions. Mainly how the wrappers will call the other functions.

---
+++ BspViewer/BspViewer.cpp
@@ -0,0 +1,37 @@
+#include "BspViewer.h"
+#include <string>
+
+BspViewer::BspViewer() {
+
+
+}
+
+BspViewer::BspViewer(std::string file_name) {
+	m_file_name = file_name;
+	open();
+
+}
+
+void BspViewer::open(std::string file_name) {
+	m_file_name = file_name;
+	open();
+
+}
+
+void BspViewer::open() {
+	m_file_ptr.open(m_file_name);
+
+}
+
+void BspViewer::render() {
+
+}
+
+void BspViewer::render(std::string file_name) {
+	open(file_name);
+	render();
+}
+
+BspViewer::~BspViewer() {
+
+}
--- BspViewer/BspViewer.h
+++ BspViewer/BspViewer.h
@@ -2,6 +2,8 @@
 #define BSP_VIEWER_H
 
 #include "BspProcessor.h"
+#include <string>
+#include <fstream>
 
 // This will use opengl and/or imgui to generate a preview of the bsp
 //	I don't know how any of this works :)
@@ -11,6 +13,7 @@ public:
 	BspViewer();
 	BspViewer(std::string file_name);
 
+	// Wrapper functions :)
 	void open(std::string file_name);
 	void open();
 
@@ -19,7 +22,13 @@ public:
 
 	~BspViewer();
 private:
+	std::string m_file_name;
+	std::ifstream m_file_ptr;
 	BspProcessor m_map_data;
+
+	// Reads the current file
+	//	Will use BspProcessor.h stuff here.
+	void read();
 
 };
 
