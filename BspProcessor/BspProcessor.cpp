6e3ce8aa9bbbd70008089e71c5c0511ff7fe3a6e
a1ineman <alineman123456555@gmail.com>
2021-11-07 22:29:35 -0500

    Added a bunch more structs. Also working on trying to read a single edge. It looks like you can read one edge at a time. But reading the whole array in doesn't work.

--- BspProcessor/BspProcessor.cpp
+++ BspProcessor/BspProcessor.cpp
@@ -15,12 +15,8 @@ BspProcessor::BspProcessor(std::string file_name) {
 
 }
 
-void BspProcessor::io ( std::fstream & file_stream, std::function < void ( std::fstream&, char *, std::streamsize ) > file_op ) {
-	// Write or read the header of the bsp
-	//file_op(file_stream, (char*) m_head, sizeof(bsp_header));
-	//file_op(file_stream, buff, 10);
-	file_op(file_stream, reinterpret_cast < char * > ( &m_head ), sizeof(bsp_header));
-
+void BspProcessor::io(std::fstream& file_stream, std::function < void(std::fstream&, char*, std::streamsize) > file_op) {
+	file_op(file_stream, reinterpret_cast <char*> (&m_head), sizeof(bsp_header));
 
 	std::cout << "Magic Number: " << m_head.magic << std::endl;
 	std::cout << "Version Number: " << m_head.version << std::endl;
@@ -28,15 +24,42 @@ void BspProcessor::io ( std::fstream & file_stream, std::function < void ( std::
 	std::cout << "Lumps: {" << std::endl;
 	for (int iter = 0; iter < VHEADERLUMPS; iter++)
 		std::cout << "\t" << BSP_FILE::lump_names.at(static_cast <BSP_FILE::LUMP> (iter))
-			<< ": offset = " << m_head.lumps[iter].offset
-			<< ", length = " << m_head.lumps[iter].length
-			<< ", version = " << m_head.lumps[iter].version
-			<< ", lump ID = " << m_head.lumps[iter].lmpID
-			<< std::endl;
+		<< ": offset = " << m_head.lumps[iter].offset
+		<< ", length = " << m_head.lumps[iter].length
+		<< ", version = " << m_head.lumps[iter].version
+		<< ", lump ID = " << m_head.lumps[iter].lmpID
+		<< std::endl;
 	std::cout << "}" << std::endl;
-	//std::cout << std::to_string(m_head.magic) << std::endl;
-	//std::cout << VBSPHEADER << std::endl;
-	//std::cout << VBSPHEADERBIG << std::endl;
+
+	std::cout << "Size of edge: " << sizeof(bsp_edge) << std::endl;
+	std::cout << "I expect 432 Edges" << std::endl;
+	
+	
+	file_stream.seekg(m_head.lumps[BSP_FILE::EDGES].offset, std::ios::beg);
+	bsp_edge test_edge;
+	file_op(file_stream, reinterpret_cast <char*> (&test_edge), sizeof(bsp_edge));
+	std::cout << std::to_string(test_edge.vert[0]) << ", " << std::to_string(test_edge.vert[1]) << std::endl;	// There we go
+
+	file_op(file_stream, reinterpret_cast <char*> (&test_edge), sizeof(bsp_edge));
+	std::cout << std::to_string(test_edge.vert[0]) << ", " << std::to_string(test_edge.vert[1]) << std::endl;	// There we go
+	file_op(file_stream, reinterpret_cast <char*> (&test_edge), sizeof(bsp_edge));
+	std::cout << std::to_string(test_edge.vert[0]) << ", " << std::to_string(test_edge.vert[1]) << std::endl;	// There we go
+	file_op(file_stream, reinterpret_cast <char*> (&test_edge), sizeof(bsp_edge));
+	std::cout << std::to_string(test_edge.vert[0]) << ", " << std::to_string(test_edge.vert[1]) << std::endl;	// There we go
+
+
+
+	/*
+	// Trying to read in edges
+	file_stream.seekg(m_head.lumps[BSP_FILE::EDGES].offset, std::ios::beg);
+	uint32_t num_edges = m_head.lumps[BSP_FILE::EDGES].length / sizeof(bsp_edge);
+	m_edges = new bsp_edge[num_edges];
+	// I think this should work
+	file_op(file_stream, reinterpret_cast <char*> (&m_edges), m_head.lumps[BSP_FILE::EDGES].length);
+	//file_op(file_stream, reinterpret_cast < char * > (&m_edges), sizeof(bsp_edge) * num_edges);
+	std::cout << num_edges << std::endl;
+	std::cout << std::to_string(m_edges[0].vert[0]) << ", " << std::to_string(m_edges[0].vert[1]) << std::endl;
+	*/
 }
 
 void BspProcessor::read_from_file(std::string file_path) {
@@ -48,7 +71,7 @@ void BspProcessor::read_from_file(std::string file_path) {
 	
 
 	std::fstream fs;
-	fs.open(file_path, std::fstream::in); //| std::fstream::binary);
+	fs.open(file_path, std::fstream::in | std::fstream::binary);
 	if (fs.is_open()) {
 		std::cout << "File opened: " << file_path << std::endl;
 		this->io(fs, read_f);
--- BspProcessor/BspProcessor.h
+++ BspProcessor/BspProcessor.h
@@ -5,6 +5,7 @@
 #include <fstream>		// For reading and writing the bsp file
 #include <cstdint>		// For specific int types
 #include <functional>
+#include <vector>		// For keeping lump values stored.
 
 // Big-endian
 //	How do I know when to use which?
@@ -46,11 +47,47 @@ struct bsp_header {
 struct bsp_plane {
 	point3f normal;
 	float	  dist;
-	int		  type;
+	int32_t		  type;
+};
+
+struct surfedge {
+	int32_t edge;	// Index into edge array.
+					//	**** Need to take abs of this value before using it to index.
+					// Positive means first to second vertx
+					//	Negative means second to first vertx
 };
 
 struct bsp_edge {
 	uint8_t vert[2];		// Index into vertex array
+};
+
+struct bsp_face {
+	uint8_t plane_ID;
+	int8_t  side;	// I don't remember somehow this tells you what side of the plane the texture is on.
+					//	Negative means something special. The oposite side of the plane normal? Or something.
+	int8_t	node_bool;  // Pontential values 1 and 0 meaning is this is a node and not a leaf.
+	int32_t first_edge; // index into surfedge
+	int8_t	num_edge;	// Number of surfedges in the surfedge array.
+	int8_t	tex_info;	// index into texture lump
+	int8_t	disp_info;	// displacement info??
+	int8_t	surf_fog_vol_ID;	// If the wiki's confused then I'm ??????????
+	unsigned char styles[4];	// Switchable lighting info????
+	int32_t	light_off;		// Offset into lightmap lump??? This sounds like this is an index not offset. Or this is going to get real messy.
+	float		 area;		// face are in units^2
+	int32_t	lightmap_text_min_in_lux[2];	// Bro wtf even is this. LUX?
+	int32_t	lightmap_text_siz_in_lux[2];	// Again :) very nice.
+	int32_t	orig_face;		// original face this was split from?
+	uint8_t	num_prims;		// Number of primitives, oh boy, that's pretty sweet :)
+	uint8_t st_prim_ID;		// first primitive index???? I really don't know what id is sounds like an index.
+	uint32_t smooth_group;	// Lightmap smoothing group.
+};
+
+struct bsp_texinfo {
+	float	texture_vecs[2][4];		//	[start/end] [xyz offset]
+	float	lightmp_vecs[2][4];		//	[start/end] [xyz offset] - Length is unit texels????/area????
+	int32_t flags;					//	I don't know some flags for some shit, who tf knows.
+	int32_t texdata;				//	Pointer to texture name, size, etc?????
+
 };
 
 class BspProcessor {
@@ -72,6 +109,14 @@ private:
 	// Not sure if the file really even has to be saved once it is loaded into memory. Can probably just forget about this.
 	//std::ifstream m_bsp_file;
 	bsp_header m_head;
+
+	bsp_edge* m_edges;
+	//std::vector < bsp_edge > m_edges_wut;
+
+	// Probably should add like a allocated flag.
+	//	So I can tell when I need to delete the arrays. and reallocate.
+	bool alloc;
+
 	//char buff;
 
 
