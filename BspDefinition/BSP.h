0c80dd486cea701b4a9a0453360e37d629c20ab3
a1ineman <alineman123456555@gmail.com>
2021-11-08 20:03:18 -0500

    Ok highlighting in io function came back. I guess it might be fixed. Adding various structs. There's a small chance that the io function will actually work now. No promises :)

--- BspDefinition/BSP.h
+++ BspDefinition/BSP.h
@@ -1,14 +1,16 @@
 
+#include <iostream>
 #include <string>
 #include <functional>
 #include <fstream>
+#include <vector>
 
 namespace BSP {
 	namespace {
 		template < typename bsp_struct_T >
 		void struct_io(std::fstream& file_stream,
-			std::function < void(std::fstream&, char*, std::streamsize) > file_op,
-			std::vector< bsp_struct_T >& bsp_vec, int lump_indx) ) {
+					   std::function < void(std::fstream&, char*, std::streamsize) > file_op,
+					   std::vector< bsp_struct_T >& bsp_vec, int lump_indx) ) {
 			unsigned int num_structs = m_head.lumps[lump_indx].length / sizeof(bsp_struct_T);
 			// Check for error
 			if (sizeof(bsp_struct_T) * num_structs != m_head.lumps[lump_indx].length) {
@@ -24,34 +26,50 @@ namespace BSP {
 		
 		//////////////////////////////////////////////////////// This is fucked :(
 		// Why doesn't this one get all the nice highlighting????
-		void io(std::fstream& file_stream, std::function < void(std::fstream&, char*, std::streamsize) > file_op), bsp in_bsp) {
-			file_op(file_stream, reinterpret_cast <char*> (&in_bsp.m_header), sizeof(bsp_header));
+		void io(std::fstream& file_stream, std::function < void(std::fstream&, char*, std::streamsize) > file_op, bsp in_bsp) {
+			file_op(file_stream, reinterpret_cast <char*> (&in_bsp.m_head), sizeof(bsp_header));
 
-			std::cout << "Magic Number: " << m_head.magic << std::endl;
-			std::cout << "Version Number: " << m_head.version << std::endl;
-			std::cout << "Revision Number: " << m_head.map_revision << std::endl;
+			std::cout << "Magic Number: " << in_bsp.m_head.magic << std::endl;
+			std::cout << "Version Number: " << in_bsp.m_head.version << std::endl;
+			std::cout << "Revision Number: " << in_bsp.m_head.map_revision << std::endl;
 			std::cout << "Lumps: {" << std::endl;
 			for (int iter = 0; iter < VHEADERLUMPS; iter++)
 				std::cout << "\t" << BSP_FILE::lump_names.at(static_cast <BSP_FILE::LUMP> (iter))
-				<< ": offset = " << m_head.lumps[iter].offset
-				<< ", length = " << m_head.lumps[iter].length
-				<< ", version = " << m_head.lumps[iter].version
-				<< ", lump ID = " << m_head.lumps[iter].lmpID
+				<< ": offset = " << in_bsp.m_head.lumps[iter].offset
+				<< ", length = " << in_bsp.m_head.lumps[iter].length
+				<< ", version = " << in_bsp.m_head.lumps[iter].version
+				<< ", lump ID = " << in_bsp.m_head.lumps[iter].lmpID
 				<< std::endl;
 			std::cout << "}" << std::endl;
 
-			struct_io(file_stream, file_op, m_edges, BSP_FILE::EDGES);
-			struct_io(file_stream, file_op, m_vertexes, BSP_FILE::VERTEXES);
+			struct_io(file_stream, file_op, in_bsp.m_edges, BSP_FILE::EDGES);
+			struct_io(file_stream, file_op, in_bsp.m_vertexes, BSP_FILE::VERTEXES);
 
 			std::cout << "Num edges: " << m_edges.size() << std::endl;
-			for (int i = 0; i < m_edges.size(); i++)
-				std::cout << i << ": " << m_edges[i].vert[0] << " - x=" << m_vertexes[m_edges[i].vert[0]].x << " y=" << m_vertexes[m_edges[i].vert[0]].y << " z=" << m_vertexes[m_edges[i].vert[0]].z
-				<< ", " << m_edges[i].vert[1] << " - x=" << m_vertexes[m_edges[i].vert[1]].x << " y=" << m_vertexes[m_edges[i].vert[1]].y << " z=" << m_vertexes[m_edges[i].vert[1]].z
+			for (int i = 0; i < in_bsp.m_edges.size(); i++)
+				std::cout << i << ": " << in_bsp.m_edges[i].vert[0] << " - x=" << in_bsp.m_vertexes[m_edges[i].vert[0]].x << " y=" << in_bsp.m_vertexes[m_edges[i].vert[0]].y << " z=" << in_bsp.m_vertexes[m_edges[i].vert[0]].z
+				<< ", " << in_bsp.m_edges[i].vert[1] << " - x=" << in_bsp.m_vertexes[m_edges[i].vert[1]].x << " y=" << in_bsp.m_vertexes[m_edges[i].vert[1]].y << " z=" << in_bsp.m_vertexes[m_edges[i].vert[1]].z
 				<< std::endl;
 
 		}
 
 	} /* anonymous namespace */
+
+	struct point3f {
+		float x;
+		float y;
+		float z;
+	};
+	struct point3s {
+		short x;
+		short y;
+		short z;
+	};
+
+	struct bsp_edge {
+		uint16_t vert[2];		// Index into vertex array
+	};
+
 	struct bsp_header {
 		char		magic[4];
 		int32_t		version;
@@ -69,7 +87,9 @@ namespace BSP {
 	// All of the things that are contained in a bsp.
 	//	This will get passed into the read/write functions.
 	struct bsp {
-		struct m_header;
+		struct m_head;
+		std::vector < bsp_edge > m_edges;
+		std::vector < point3f >  m_vertexes;
 
 	};
 
